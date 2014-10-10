06bc46c4a8dad7083c42e0583eb2482d3502e7de
Yichi Zhang <zhang-yi-chi@hotmail.com>
2014-10-11 02:06:24 +1100

    Some more updates
    Some more updates

--- YZLibrary/NSArray+YZLibrary.m
+++ YZLibrary/NSArray+YZLibrary.m
@@ -1,10 +1,16 @@
-//
-//  NSArray+YZLibrary.m
-//  twoamlockout
-//
-//  Created by Yichi Zhang on 21/04/13.
-//  Copyright (c) 2013 Yichi Zhang. All rights reserved.
-//
+/*
+ 
+ Copyright (c) 2014 Yichi Zhang
+ https://github.com/yichizhang
+ zhang-yi-chi@hotmail.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
 
 #import "NSArray+YZLibrary.h"
 
@@ -12,18 +18,18 @@
 
 int currentIndex = 0;
 
--(id)randomObject{
+-(id)yz_randomObject{
     return [ self objectAtIndex: (int) ( arc4random() % [self count] ) ];
 }
 
--(id)nextObject{
+-(id)yz_nextObject{
     if (currentIndex >= [self count]) {
         currentIndex = 0;
     }
     return [self objectAtIndex:currentIndex++];
 }
 
-- (NSArray *) shuffled
+- (NSArray *)yz_shuffled
 {
 	// create temporary autoreleased mutable array
 	NSMutableArray *tmpArray = [NSMutableArray arrayWithCapacity:[self count]];
