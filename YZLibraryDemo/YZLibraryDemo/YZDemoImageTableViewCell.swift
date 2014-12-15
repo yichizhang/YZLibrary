922351cf4156216b556690a07e33af3f0063f3e0
Yichi Zhang <zhang-yi-chi@hotmail.com>
2014-12-15 20:36:39 +1100

    Updated demo project; it's partly Swift now

---
+++ YZLibraryDemo/YZLibraryDemo/YZDemoImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  YZDemoImageTableViewCell.swift
+//  YZLibraryDemo
+//
+//  Created by Yichi on 15/12/2014.
+//  Copyright (c) 2014 yichizhang. All rights reserved.
+//
+
+import UIKit
+
+class YZDemoImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
