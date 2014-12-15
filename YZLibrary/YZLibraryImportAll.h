e39591b80c49a88031dbf7e0011b31b8d4de6488
Yichi Zhang <zhang-yi-chi@hotmail.com>
2014-12-15 20:36:01 +1100

    Added Import All header for Swift

---
+++ YZLibrary/YZLibraryImportAll.h
@@ -0,0 +1,35 @@
+/*
+ 
+ Copyright (c) 2014 Yichi Zhang
+ https://github.com/yichizhang
+ zhang-yi-chi@hotmail.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
+
+#ifndef YZLibraryDemo_YZLibraryImportAll_h
+#define YZLibraryDemo_YZLibraryImportAll_h
+
+#import "NSArray+YZLibrary.h"
+#import "NSDictionary+YZLibrary.h"
+#import "NSString+YZLibrary.h"
+#import "UIButton+YZLibrary.h"
+#import "UICollectionViewCell+YZLibrary.h"
+#import "UIColor+YZLibrary.h"
+#import "UIImage+YZLibrary.h"
+#import "UIImageView+YZLibrary.h"
+#import "UITableViewCell+YZLibrary.h"
+#import "UIView+YZLibrary.h"
+#import "UIViewController+YZLibrary.h"
+#import "YZFileHelper.h"
+#import "YZLibrary.h"
+#import "YZRandom.h"
+#import "YZStringHelper.h"
+#import "YZTimeHelper.h"
+
+#endif
