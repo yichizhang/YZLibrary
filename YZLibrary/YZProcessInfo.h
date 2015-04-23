4f8a8931c44e7f966f37bdb2aeae98d8c6465c6c
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-04-23 10:19:24 +1000

    Added comments.

--- YZLibrary/YZProcessInfo.h
+++ YZLibrary/YZProcessInfo.h
@@ -22,22 +22,29 @@ typedef struct {
 
 @interface YZProcessInfo : NSObject
 
+/** A string containing the version of the current device. (read-only)
+ 
+ */
 @property (readonly, copy) NSString *operatingSystemVersionString;
+
+/** The version of the current device. (read-only)
+ 
+ */
 @property (readonly) YZOperatingSystemVersion operatingSystemVersion;
 
 
-/** TODO:
+/** Returns an instance of YZProcessInfo.
  
  */
 + (YZProcessInfo *)processInfo;
 
 
-/** TODO:
+/** Returns a Boolean value indicating whether the version of the operating system of the device is the same or later than the given version.
  
  */
 - (BOOL)isOperatingSystemAtLeastVersion:(YZOperatingSystemVersion)version;
 
-/** TODO:
+/** Returns a Boolean value indicating whether the major version of the operating system of the device is the same or later than the given major version. 
  
  */
 - (BOOL)isOperatingSystemAtLeastMajorVersion:(NSInteger)majorVersion;
