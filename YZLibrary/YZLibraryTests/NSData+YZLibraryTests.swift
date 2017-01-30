39192d109b20dd1e50eafa2588906e7f9d1870f9
Yichi Zhang <zhang-yi-chi@hotmail.com>
2017-01-30 17:14:07 +1000

    Add tests from previous YZLibraryDemo project; integrate Quick and Nimble with Carthage

---
+++ YZLibrary/YZLibraryTests/NSData+YZLibraryTests.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright (c) 2017-present, Yichi Zhang.
+//
+//  This source code is licensed under the MIT-style license found
+//  in the LICENSE file in the root directory of this source tree.
+//
+
+import Quick
+import Nimble
+
+private func createData(with s: String) -> NSData {
+  return s.data(using: .utf8) as! NSData
+}
+
+private func createData(bundle: Bundle, fileName: String, type: String) -> NSData? {
+  if let path = bundle.path(forResource: fileName, ofType: type) {
+    return NSData(contentsOfFile: path)
+  }
+  return nil
+}
+
+class MD5Spec: QuickSpec {
+  override func spec() {
+    describe("MD5 method") {
+      var data: NSData?
+      let bundle = Bundle(for: type(of: self))
+      
+      it("returns correct result for strings") {
+        data = createData(with: "Alice in Wonderland")
+        expect(data?.yz_MD5String()).to(equal("50e9fcc3911994a2f84eaf8c12c49b5b"))
+      }
+      
+      it("returns correct result for a pdf file") {
+        data = createData(bundle: bundle, fileName: "test_pdf", type: "pdf")
+        expect(data?.yz_MD5String()).to(equal("61a72a52188553124e9bd01f63a6e1ca"))
+      }
+    }
+  }
+}
--- YZLibrary/YZLibraryTests/YZLibraryTests.m
+++ YZLibrary/YZLibraryTests/YZLibraryTests.m
@@ -1,39 +1,141 @@
 //
-//  YZLibraryTests.m
-//  YZLibraryTests
+//  Copyright (c) 2017-present, Yichi Zhang.
 //
-//  Created by Yichi Zhang on 25/09/2016.
-//  Copyright © 2016 Yichi Zhang. All rights reserved.
+//  This source code is licensed under the MIT-style license found
+//  in the LICENSE file in the root directory of this source tree.
 //
 
 #import <XCTest/XCTest.h>
+#import <YZLibrary/YZLibrary.h>
 
-@interface YZLibraryTests : XCTestCase
+@interface YZLibrayTests : XCTestCase
 
 @end
 
-@implementation YZLibraryTests
+@implementation YZLibrayTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testIsEmpty
+{
+    NSArray *a = @[];
+    XCTAssertTrue(YZ_IS_EMPTY(a));
+    
+    NSArray *b = @[ @"" ];
+    XCTAssertTrue(YZ_IS_EMPTY(b) == NO);
+    
+    NSArray *c = nil;
+    XCTAssertTrue(YZ_IS_EMPTY(c));
+    
+    NSDictionary *d = @{};
+    XCTAssertTrue(YZ_IS_EMPTY(d));
+    
+    NSString *e = @"";
+    XCTAssertTrue(YZ_IS_EMPTY(e));
+    
+    NSString *f = nil;
+    XCTAssertTrue(YZ_IS_EMPTY(f));
+    
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testEmailValidation {
+    
+    XCTAssertTrue([@"" yz_isValidEmail] == NO);
+    XCTAssertTrue([@"myemail" yz_isValidEmail] == NO);
+    XCTAssertTrue([@"me@email" yz_isValidEmail] == NO);
+    XCTAssertTrue([@"me@email.c" yz_isValidEmail] == NO);
+    
+    XCTAssertTrue([@"me@email.com" yz_isValidEmail] == YES);
+    XCTAssertTrue([@"me@e.co" yz_isValidEmail] == YES);
+}
+
+- (void)testCGRectWithCenterAndSize {
+    
+    CGRect rect;
+    
+    rect = YZ_CGRectWithCenterAndSize(160.f, 50.f, 40.f, 20.f);
+    
+    XCTAssertEqual(rect.origin.x, 140.f);
+    XCTAssertEqual(rect.origin.y, 40.f);
+    XCTAssertEqual(rect.size.width, 40.f);
+    XCTAssertEqual(rect.size.height, 20.f);
+    
+    rect = YZ_CGRectWithCenterAndSize(40.f, 20.f, 120.f, 50.f);
+    
+    XCTAssertEqual(rect.origin.x, -20.f);
+    XCTAssertEqual(rect.origin.y, -5.f);
+    XCTAssertEqual(rect.size.width, 120.f);
+    XCTAssertEqual(rect.size.height, 50.f);
+}
+
+- (void)testCamelCaseAndUnderscoreCaseString {
+    
+    NSArray *testStrings =
+    @[
+      @"1984.8.8 Dingo Show7b",
+      @"-Balsamic vinegar flavoured Vegemite-",
+      @"Python's secrets1111 23 44",
+      @"Objective-C coding convention",
+      @"UIViewController Secrets",
+      @"{Test failed}, AA 12345",
+      @"Location: #23406A",
+      @"Use `objc_getAssociatedObject(self, &AssociatedKeys.DescriptiveName)`",
+      @"my email is - me@email.com",
+      ];
+    
+    NSInteger index;
+    
+    NSArray *camelCaseExpectedStrings =
+    @[
+      @"198488DingoShow7B",
+      @"BalsamicVinegarFlavouredVegemite",
+      @"PythonsSecrets11112344",
+      @"ObjectiveCCodingConvention",
+      @"UIViewControllerSecrets",
+      @"TestFailedAA12345",
+      @"Location23406A",
+      @"UseObjcGetAssociatedObjectSelfAssociatedKeysDescriptiveName",
+      @"myEmailIsMeEmailCom",
+      ];
+    
+    index = 0;
+    for (NSString* expected in camelCaseExpectedStrings){
+        NSString *actual = [testStrings[index] yz_camelCaseStringWithOptions:YZStringConversionRetainOriginalLetterCase];
+        XCTAssertTrue([actual isEqualToString:expected],
+                      @"Strings are not equal. Expected: `%@`, actual: `%@`", expected, actual);
+        index++;
+    }
+    
+    NSArray *underscoreCaseExpectedStrings =
+    @[
+      @"1984_8_8_dingo_show_7_b",
+      @"balsamic_vinegar_flavoured_vegemite",
+      @"pythons_secrets_1111_23_44",
+      @"objective_c_coding_convention",
+      @"u_i_view_controller_secrets",
+      @"test_failed_a_a_12345",
+      @"location_23406_a",
+      @"use_objc_get_associated_object_self_associated_keys_descriptive_name",
+      @"my_email_is_me_email_com",
+      ];
+    
+    index = 0;
+    for (NSString* expected in underscoreCaseExpectedStrings){
+        NSString *actual = [testStrings[index] yz_underscoreCaseStringWithOptions:YZStringConversionNone];
+        XCTAssertTrue([actual isEqualToString:expected],
+                      @"Strings are not equal. Expected: `%@`, actual: `%@`", expected, actual);
+        index++;
+    }
 }
 
 @end
