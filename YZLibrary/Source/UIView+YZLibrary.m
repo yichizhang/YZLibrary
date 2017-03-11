3a504350a57e700c8351b7564e19121f88f07fc5
Yichi Zhang <zhang-yi-chi@hotmail.com>
2017-03-12 03:16:58 +1000

    Format UIView+YZLibrary.m

--- YZLibrary/Source/UIView+YZLibrary.m
+++ YZLibrary/Source/UIView+YZLibrary.m
@@ -32,134 +32,136 @@
 
 + (nullable instancetype)yz_loadFromNib
 {
-    return [[self class] yz_loadFromNibNamed:[self yz_className] bundle:nil];
+  return [[self class] yz_loadFromNibNamed:[self yz_className] bundle:nil];
 }
 
 + (nullable instancetype)yz_loadFromNibNamed:(nonnull NSString *)nibName bundle:(nullable NSBundle *)bundleOrNil
 {
-    static UINib *__nib;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        __nib = [UINib nibWithNibName:nibName bundle:bundleOrNil];
-    });
-
-    return [[__nib instantiateWithOwner:nil options:nil] firstObject];
+  static UINib *__nib;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    __nib = [UINib nibWithNibName:nibName bundle:bundleOrNil];
+  });
+  
+  return [[__nib instantiateWithOwner:nil options:nil] firstObject];
 }
 
 - (void)yz_bringToFront
 {
-    [[self superview] bringSubviewToFront:self];
+  [self.superview bringSubviewToFront:self];
 }
 
 - (nonnull UIImage *)yz_captureImage
 {
-    return [self yz_captureImageWithScaleFactor:
-            [UIScreen mainScreen].scale
-    ];
+  return [self yz_captureImageWithScaleFactor:UIScreen.mainScreen.scale];
 }
 
 - (nonnull UIImage *)yz_captureImageWithScaleFactor:(CGFloat)scaleFactor
 {
-    UIGraphicsBeginImageContextWithOptions(self.bounds.size,
-            NO,
-            scaleFactor
-    );
-    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-
-    return image;
+  UIGraphicsBeginImageContextWithOptions(self.bounds.size,
+                                         NO,
+                                         scaleFactor
+                                         );
+  [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  
+  return image;
 }
 
 - (nullable UICollectionViewCell *)yz_parentCollectionViewCell
 {
-    id viewSuperView = [self superview];
-    while (viewSuperView != nil) {
-        if ([viewSuperView isKindOfClass:[UICollectionViewCell class]]) {
-            return (UICollectionViewCell *) viewSuperView;
-        }
-        else {
-            viewSuperView = [viewSuperView superview];
-        }
+  id viewSuperView = [self superview];
+  while (viewSuperView != nil) {
+    if ([viewSuperView isKindOfClass:[UICollectionViewCell class]]) {
+      return (UICollectionViewCell *) viewSuperView;
     }
-    return nil;
+    else {
+      viewSuperView = [viewSuperView superview];
+    }
+  }
+  return nil;
 }
 
 - (nullable UITableViewCell *)yz_parentTableViewCell
 {
-    id viewSuperView = [self superview];
-    while (viewSuperView != nil) {
-        if ([viewSuperView isKindOfClass:[UITableViewCell class]]) {
-            return (UITableViewCell *) viewSuperView;
-        }
-        else {
-            viewSuperView = [viewSuperView superview];
-        }
+  id viewSuperView = [self superview];
+  while (viewSuperView != nil) {
+    if ([viewSuperView isKindOfClass:[UITableViewCell class]]) {
+      return (UITableViewCell *) viewSuperView;
     }
-    return nil;
+    else {
+      viewSuperView = [viewSuperView superview];
+    }
+  }
+  return nil;
 }
 
 - (void)yz_preloadKeyboard
 {
-    UITextField *tempTextField = [[UITextField alloc] init];
-    [self addSubview:tempTextField];
-    [tempTextField becomeFirstResponder];
-    [tempTextField resignFirstResponder];
-    [tempTextField removeFromSuperview];
+  UITextField *tempTextField = [[UITextField alloc] init];
+  [self addSubview:tempTextField];
+  [tempTextField becomeFirstResponder];
+  [tempTextField resignFirstResponder];
+  [tempTextField removeFromSuperview];
 }
 
 - (void)yz_setupConstraintsAdhereToEdgesOfSuperview
 {
-    [self yz_setupConstraintsAdhereToEdgesOfSuperviewWithInsets:UIEdgeInsetsZero];
+  [self yz_setupConstraintsAdhereToEdgesOfSuperviewWithInsets:UIEdgeInsetsZero];
 }
 
 - (void)yz_setupConstraintsAdhereToEdgesOfSuperviewWithInsets:(UIEdgeInsets)inset
 {
-    UIView *contentView = self.superview;
-    UIView *itemView = self;
-
-    self.translatesAutoresizingMaskIntoConstraints = NO;
-
-    NSLayoutConstraint *topConstraint =
-            [NSLayoutConstraint constraintWithItem:itemView
-                                         attribute:NSLayoutAttributeTop
-                                         relatedBy:NSLayoutRelationEqual
-                                            toItem:contentView
-                                         attribute:NSLayoutAttributeTop
-                                        multiplier:1.0f
-                                          constant:inset.top];
-
-    NSLayoutConstraint *leadingConstraint =
-            [NSLayoutConstraint constraintWithItem:itemView
-                                         attribute:NSLayoutAttributeLeading
-                                         relatedBy:NSLayoutRelationEqual
-                                            toItem:contentView
-                                         attribute:NSLayoutAttributeLeading
-                                        multiplier:1.0f
-                                          constant:inset.left];
-
-    NSLayoutConstraint *bottomConstraint =
-            [NSLayoutConstraint constraintWithItem:contentView
-                                         attribute:NSLayoutAttributeBottom
-                                         relatedBy:NSLayoutRelationEqual
-                                            toItem:itemView
-                                         attribute:NSLayoutAttributeBottom
-                                        multiplier:1.0f
-                                          constant:inset.bottom];
-
-    NSLayoutConstraint *trailingConstraint =
-            [NSLayoutConstraint constraintWithItem:contentView
-                                         attribute:NSLayoutAttributeTrailing
-                                         relatedBy:NSLayoutRelationEqual
-                                            toItem:itemView
-                                         attribute:NSLayoutAttributeTrailing
-                                        multiplier:1.0f
-                                          constant:inset.right];
-
-    [contentView addConstraint:topConstraint];
-    [contentView addConstraint:leadingConstraint];
-    [contentView addConstraint:bottomConstraint];
-    [contentView addConstraint:trailingConstraint];
+  UIView *contentView = self.superview;
+  UIView *itemView = self;
+  
+  self.translatesAutoresizingMaskIntoConstraints = NO;
+  
+  NSLayoutConstraint *topConstraint =
+  [NSLayoutConstraint
+   constraintWithItem:itemView
+   attribute:NSLayoutAttributeTop
+   relatedBy:NSLayoutRelationEqual
+   toItem:contentView
+   attribute:NSLayoutAttributeTop
+   multiplier:1.0f
+   constant:inset.top];
+  
+  NSLayoutConstraint *leadingConstraint =
+  [NSLayoutConstraint
+   constraintWithItem:itemView
+   attribute:NSLayoutAttributeLeading
+   relatedBy:NSLayoutRelationEqual
+   toItem:contentView
+   attribute:NSLayoutAttributeLeading
+   multiplier:1.0f
+   constant:inset.left];
+  
+  NSLayoutConstraint *bottomConstraint =
+  [NSLayoutConstraint
+   constraintWithItem:contentView
+   attribute:NSLayoutAttributeBottom
+   relatedBy:NSLayoutRelationEqual
+   toItem:itemView
+   attribute:NSLayoutAttributeBottom
+   multiplier:1.0f
+   constant:inset.bottom];
+  
+  NSLayoutConstraint *trailingConstraint =
+  [NSLayoutConstraint
+   constraintWithItem:contentView
+   attribute:NSLayoutAttributeTrailing
+   relatedBy:NSLayoutRelationEqual
+   toItem:itemView
+   attribute:NSLayoutAttributeTrailing
+   multiplier:1.0f
+   constant:inset.right];
+  
+  [contentView addConstraint:topConstraint];
+  [contentView addConstraint:leadingConstraint];
+  [contentView addConstraint:bottomConstraint];
+  [contentView addConstraint:trailingConstraint];
 }
 
 @end
